f456b0472e6144af1550b392f0a3d9eb85ae4132
ikemanep1 <ike.esquivelpilloud@gmail.com>
2020-01-17 21:50:12 -0800

    add complete working logic

--- js/scripts.js
+++ js/scripts.js
@@ -9,15 +9,25 @@ $(document).ready(function() {
     var identity = parseInt($("#ideology").val());
     var results = resolution + weapon + society + companion + identity;
     if (results > 0 && results <= 5) {
-      $(".result").text("You are a white planeswalker. You value order and society, at the expense of individualism.");
+      $(".result").children("p").first().remove();
+      $('.result').prepend('<p>You are a white planeswalker. You value order and society, at the expense of individualism.</p>');
+      $(".result").show();
     } else if (results > 5 && results <= 10 ) {
-      $(".result").text("You are a blue planeswalker. You value knowledge and perfection at the expense of sheer power.");
+      $(".result").children("p").first().remove();
+      $('.result').prepend('<p>You are a blue planeswalker. You value knowledge and perfection, at the expense of raw power.</p>');
+      $(".result").toggle();
     } else if (results > 10 && results <= 15 ) {
-      $(".result").text("You are a black planeswalker. You value power and excess at the expense of morality.");
+      $(".result").children("p").first().remove();
+      $('.result').prepend('<p>You are a black planeswalker. You value power and excess, at the expense of morality.</p>');
+      $(".result").toggle();
     } else if (results > 15 && results <= 20 ) {
-      $(".result").text("You are a red planeswalker. You value passion and freedom at the expense of order.");
+      $(".result").children("p").first().remove();
+      $('.result').prepend('<p>You are a red planeswalker. You value passion and freedom, at the expense of order.</p>');
+      $(".result").toggle();
     } else if (results > 20 && results <= 25 ) {
-      $(".result").text("You are a green planeswalker. You value nature and instinct at the expense of technology.");
+      $(".result").children("p").first().remove();
+      $('.result').prepend('<p>You are a green planeswalker. You value instinct and adaptation, at the expense of technology.</p>');
+      $(".result").toggle();
     } else {
    alert('Please fill out the entire form.');
  }
