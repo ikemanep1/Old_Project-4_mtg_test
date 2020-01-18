7d63b8e3dcde42077dd730708753cf230fac78ab
ikemanep1 <ike.esquivelpilloud@gmail.com>
2020-01-17 21:25:34 -0800

    continue adding front end logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,3 @@
-
-//
-// $(document).ready(function() {
-//   var age = parseInt(prompt("How old are you?"));
-//
-//   if (age > 21) {
-//     $('#drinks').show();
-//   } else if (age === 21) {
-//     alert("Now don't go crazy!");
-//     $('#drinks').show();
-//   } else {
-//     $('#under-21').show();
-//   }
-// });
-
 // front end
 $(document).ready(function() {
   $("form#mtg_test").submit(function(event) {
@@ -23,6 +8,14 @@ $(document).ready(function() {
     var companion = parseInt($("#companionship").val());
     var identity = parseInt($("#ideology").val());
     var results = resolution + weapon + society + companion + identity;
+    if (results > 0 && results <= 15) {
+      $(".result").text("You are a white planeswalker. You value order and society, at the expense of individualism.");
+    } else if (results > 15 && results <= 30 ) {
+      $(".result").text("You are a blue planeswalker. You value knowledge and perfection at the expense of sheer power.");
+    } else if (results > 30 && results <= 45 ) {
+      $(".result").text("You are a black planeswalker. You value power and excess at the expense of morality.");
+    } else if (results > 30 && results <= 45 ) {
+      $(".result").text("You are a black planeswalker. You value power and excess at the expense of morality.");
     $(".result").text(results);
   });
 });
